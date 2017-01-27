09aad03d03461a90d3c3b7cedef8a447610c0b1a
u4bi <u4bi@u4bi.com>
2017-01-27 22:27:19 +0000

    golden layout - low css update fix config val

--- low/js/index.js
+++ low/js/index.js
@@ -3,19 +3,28 @@ var config = {
     type: 'row',
     content: [
         {
-        type:'component',
-        componentName: 'basic_low',
-        componentState: { text: '컨테이너 1번' }
+          type:'component',
+          componentName: 'basic_low',
+          componentState: {
+            text: '컨테이너 1번',
+            test_val : '내용 1번'
+          }
         },
-      {
-        type:'component',
-        componentName: 'basic_low',
-        componentState: { text: '컨테이너 2번' }
+        {
+          type:'component',
+          componentName: 'basic_low',
+          componentState: {
+            text: '컨테이너 2번',
+            test_val : '내용 1번'
+          }
         },
-      {
-        type:'component',
-        componentName: 'basic_low',
-        componentState: { text: '컨테이너 3번' }
+        {
+          type:'component',
+          componentName: 'basic_low',
+          componentState: {
+            text: '컨테이너 3번',
+            test_val : '내용 1번'
+          }
         }
     ]
   }]
@@ -24,5 +33,8 @@ var config = {
 var myLayout = new GoldenLayout( config );
 
 myLayout.registerComponent( 'basic_low', function( container, state ){
-  container.getElement().html( '<h2>' + state.text + '</h2>');
+  container.getElement()
+    .html( '<h2>' + state.text + '</h2>'+'<span>'+state.test_val+'</span>');
 });
+
+myLayout.init();
